c0c0b2ba017d8154db8bc12dc4e274c8240a58bf
jorisjjansen <jorisjip@gmail.com>
2017-06-22 14:55:36 +0200

    megapsuh

--- app/src/main/java/com/example/joris/hoorcollegeechte/MainActivity.java
+++ app/src/main/java/com/example/joris/hoorcollegeechte/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.joris.hoorcollegeechte;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
 
 
 public class MainActivity extends Activity {
@@ -21,41 +21,21 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
+
         return super.onOptionsItemSelected(item);
     }
 
     public void getBird(View view) {
-
-        // We have to state that are intention is to open another Activity. We do so
-        // by passing a Context and the Activity that we want to open
-
         Intent getNameScreenIntent = new Intent(this, SecondScreen.class);
-
-        // We ask for the Activity to start and don't expect a result to be sent back
-        // startActivity(getNameScreenIntent);
-
-        // We use startActivityForResult when we expect a result to be sent back
-
         final int result = 1;
 
-        // To send data use putExtra with a String name followed by its value
-
         getNameScreenIntent.putExtra("callingActivity", "MainActivity");
-
         startActivityForResult(getNameScreenIntent, result);
 
     }
@@ -64,13 +44,10 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Create the TextView so I can put the users name on it
         TextView usersNameMessage = (TextView) findViewById(R.id.bird_name);
 
-        // Get the users name from the previous Activity
         String nameSentBack = data.getStringExtra("UsersName");
 
-        // Add the users name to the end of the textView
         usersNameMessage.append(" " + nameSentBack);
 
     }
@@ -78,6 +55,6 @@ public class MainActivity extends Activity {
     public void launchCamera(View view) {
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         startActivity(intent);
-
     }
+
 }
